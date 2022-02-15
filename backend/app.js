4a5c17191aa07c01165814e1cf7e33d888b0f745
Mika <mrcaron@hotmail.fr>
2022-02-15 16:53:11 +0100

    Début du projet 7
    création du dossier backend (server node) et frontend (vue cli)

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); //accéder à notre API depuis n'importe quelle origine
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  ); //ajouter les headers mentionnés aux requêtes envoyées vers notre API
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  ); //envoyer des requêtes avec les méthodes mentionnées
+  next();
+});
+
+app.use(express.json());
+
+module.exports = app;
